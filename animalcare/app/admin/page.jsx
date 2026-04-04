5dc0dee600ac5c8c96b71182e84fdcb711621f9d
Watonnum <ipae.wo@gmail.com>
2026-04-04 14:01:37 +0700

    add permission&role

---
+++ animalcare/app/admin/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center mt-12">
+      <p className="text-4xl">This Page access only ADMIN permission</p>
+    </div>
+  );
+};
+
+export default page;
