8821909000542aced90ed8ce9fca492e871b3bcd
Watonnum <ipae.wo@gmail.com>
2026-04-23 16:18:32 +0700

    remove useless file

--- animalcare/test-data.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:3000/api/services")
-  .then(res => res.json())
-  .then(data => {
-    console.log(JSON.stringify(data.slice(0, 2), null, 2));
-  }).catch(e => console.error(e));
